605a35555028adb1c1a0a7012bebab117b4d4525
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2024-02-09 11:19:29 -0300

    chore: cleanup modules from 2023-q4 edition

--- src/routes.rs
+++ src/routes.rs
@@ -1,5 +1,3 @@
-pub mod count_devs;
 pub mod customers;
-pub mod devs;
 pub mod health_check;
 pub mod transaction;
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -1,9 +1,5 @@
-mod count_devs;
-mod get_dev_by_id;
-mod get_devs_by_search_term;
 mod get_statement;
 mod health_check;
 pub mod helpers;
 mod post_customer;
-mod post_devs;
 mod post_transaction;
